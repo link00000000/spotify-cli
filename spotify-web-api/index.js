5ed99bd9986872c6689344daae5c2cc210f2d5d2
link00000000 <crandall.logan@gmail.com>
2018-05-28 20:56:01 -0400

    Added support for token saving and polling currently playing track

--- spotify-web-api/index.js
+++ spotify-web-api/index.js
@@ -1,7 +1,7 @@
 /* Modules */
 const dotenv = require('dotenv'); dotenv.config()
 const request = require('request-promise')
-const db = require('node-json-db')
+const JsonDB = require('node-json-db')
 
 /* Global variables */
 const CLIENT_ID = process.env.CLIENT_ID
@@ -12,11 +12,85 @@ const tokenHost = 'https://accounts.spotify.com'
 const tokenPath = '/api/token'
 const authorizePath = '/authorize'
 
-/* authorize fn */
-async function authorize(scopes) {
+const apiHost = 'https://api.spotify.com'
 
-    // Oauth authorization URI
-    const authorizationUri = tokenHost + authorizePath + '?' +
+const db = new JsonDB('authorization', true, false)
+
+/* Spotify Class */
+function Spotify() {
+    this.accessToken = null
+    this.refreshToken = null
+    this.expiry = null
+
+    // Set access and refresh tokens
+    this.setTokens = (tokens) => {
+        this.accessToken = tokens['access_token']
+        this.refreshToken = this.refreshToken || tokens['refresh_token']
+        this.expiry = tokens['expiry'] || (new Date()).valueOf() + (tokens['expires_in'] * 1000)
+        db.push('/spotify', tokens)
+        db.push('/spotify/expiry', this.expiry)
+    }
+
+    // Update access tokens if expired
+    this.updateAccessToken = async () => {
+        
+        // If access token is expired
+        if((new Date()).valueOf() > this.expiry) {
+
+            // Retrieve new access token using refresh token
+            let tokens = await request({
+                method: 'post',
+                url: tokenHost + tokenPath,
+                form: {
+                    'grant_type': 'refresh_token',
+                    'refresh_token': this.refreshToken
+                },
+                headers: {
+                    'Authorization': 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'),
+                    'content-type': 'application/x-www-form-urlencoded'
+                }
+            })
+            this.setTokens(JSON.parse(tokens))
+        }
+    }
+
+    // Returns the current song being listened to
+    this.getPlaybackInfo = async () => {
+        await this.updateAccessToken()
+        let res = await request({
+            method: 'get',
+            url: apiHost + '/v1/me/player',
+            headers: {
+                'Accept': 'application/json',
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + this.accessToken
+            }
+        })
+        res = JSON.parse(res)
+        return {
+            'song': res.item.name,
+            'artist': res.item.artists[0].name,
+            'progress': res.progress_ms,
+            'duration': res.item.duration_ms,
+            'isPlaying': res.is_playing
+        }
+    }
+
+    
+}
+
+/* authorize fn */
+Spotify.prototype.authorize = async (scopes) => {
+
+    // Retrieve saved tokens
+    try {
+        let tokens = db.getData('/spotify')
+        return tokens
+
+    // Authorize with spotify if tokens are not saved
+    } catch (e) {
+        // Oauth authorization URI
+        const authorizationUri = tokenHost + authorizePath + '?' +
         JSON.stringify({
             'client_id': CLIENT_ID,
             'response_type': 'code',
@@ -56,12 +130,9 @@ async function authorize(scopes) {
             'content-type': 'application/x-www-form-urlencoded'
         }
     })
-    return tokens
+    return JSON.parse(tokens)
+    }
 
 }
 
-/* saveTokens fn */
-
-
-module.exports.authorize = authorize
-module.exports.saveTokens = saveTokens
+module.exports = Spotify
